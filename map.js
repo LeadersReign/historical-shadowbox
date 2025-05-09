bcbf1f70f7f009d29e70c4949efbc55fa7a2c772
LeadersReign <leadersreign@gmail.com>
2025-05-09 16:24:44 -0500

    Update map.js

--- map.js
+++ map.js
@@ -4,7 +4,6 @@ const map = L.map('map').setView([40.8, -96.7], 6); // Nebraska
 // Add OpenStreetMap tiles
 var CartoDB_DarkMatterNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
 	maxZoom: 20
 });
 
