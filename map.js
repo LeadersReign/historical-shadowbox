4d93ba82ceea3fc6b176b19a7b2c87d45717ae52
LeadersReign <leadersreign@gmail.com>
2025-05-09 16:01:44 -0500

    Update map.js

--- map.js
+++ map.js
@@ -2,10 +2,10 @@
 const map = L.map('map').setView([40.8, -96.7], 6); // Nebraska
 
 // Add OpenStreetMap tiles
-var CartoDB_DarkMatterNoLabels = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png', {
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
-	maxZoom: 20
+var CartoDB_DarkMatterNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  subdomains: 'abcd',
+  maxZoom: 20
 });
 
 map.addLayer(CartoDB_DarkMatterNoLabels);
@@ -19,24 +19,16 @@ fetch('data.json')
       marker.bindPopup(`<strong>${event.title}</strong><br>${event.description}`);
       
       // When a marker is clicked, populate and show the sidebar
-    	marker.on('click', () => {
-  	   document.getElementById('sidebar-title').innerText = event.title;
-	   document.getElementById('sidebar-description').innerText = event.description;
-	   document.getElementById('sidebar').classList.add('active');
-	});
-
+      marker.on('click', () => {
+        document.getElementById('sidebar-title').innerText = event.title;
+        document.getElementById('sidebar-description').innerText = event.description;
+        document.getElementById('sidebar').classList.add('active');
       });
     });
   });
 
 function closeSidebar() {
   document.getElementById('close-sidebar').addEventListener('click', () => {
-  document.getElementById('sidebar').classList.remove('active');
-});
-
+    document.getElementById('sidebar').classList.remove('active');
+  });
 }
-
-closeSidebar();
-
-
-
